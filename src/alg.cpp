2a5dbb70a8018b92c05be83b615fe8c2d21da23e
IanaVatagina <vatagina.iana@gmail.com>
2024-03-15 21:47:46 +0300

    Update_ADS-4

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,19 +1,30 @@
 // Copyright 2021 NNTU-CS
-int sortArr(int *arr, int size) {
-    for (int i = 1; i < size; i++) {
-        bool isSorted = true;
-        for (int j = 0; j < size - i; j++) {
-            if (arr[j] > arr[j+1]) {
-                int param = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = param;
-                isSorted = false;
+int cbinsearch(int *arr, int size, int value) {
+    int counter = 0;
+    int lowInd = 0;
+    int highInd = size-1;
+    while (lowInd <= highInd) {
+        int middleInd = (highInd+lowInd)/2;
+        if (arr[middleInd] > value) {
+            highInd = middleInd-1;
+        } else if (arr[middleInd] < value) {
+            lowInd = middleInd+1;
+        } else {
+            counter++;
+            int nowInd = middleInd-1;
+            middleInd++;
+            while (arr[middleInd] == value) {
+                middleInd++;
+                counter++;
             }
-        }
-        if (isSorted)
+            while (arr[nowInd] == value) {
+                nowInd--;
+                counter++;
+            }
             break;
+        }
     }
-    return 0;
+    return counter;
 }
 int countPairs1(int *arr, int len, int value) {
     int counter = 0;
@@ -29,7 +40,6 @@ int countPairs1(int *arr, int len, int value) {
     return counter;
 }
 int countPairs2(int *arr, int len, int value) {
-    sortArr(arr, len);
     int counter = 0;
     int stepRight = len-1;
     while (arr[stepRight] > value)
@@ -38,22 +48,11 @@ int countPairs2(int *arr, int len, int value) {
     return counter;
 }
 int countPairs3(int *arr, int len, int value) {
-    sortArr(arr, len);
     int counter = 0;
-    int stepRight = len-1;
     int stepLeft = 0;
-    while (stepLeft <= stepRight) {
-        int summa = arr[stepLeft] + arr[stepRight];
-        if (summa > value) {
-            stepRight--;
-        } else if (summa < value) {
-            stepLeft++;
-        } else {
-            counter++;
-            while (summa == value) {
-                countPairs3(arr, stepRight, value);
-            }
-        }
+    while (stepLeft < len/2) {
+        int ostSlag = value - arr[stepLeft];
+        counter += cbinsearch(arr+1, len - stepLeft, ostSlag);
     }
     return counter;
 }
